61e2274dd21edbb76e9d7a3f9692f1353339b95a
Cooper LeBrun <cooperlebrun@gmail.com>
2024-10-10 13:34:08 -0400

    stuff

--- main.sh
+++ main.sh
@@ -1,16 +1,19 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # Script for converting web pages into markdown format
 
 # TODO:
-# - [ ] Download from url
+# - [x] Download from url
 #   - [ ] Error handling for:
 #     - [ ] 404
 #     - [ ] 403
 #     - [ ] 500
 # - [ ] Consume from stdin by default, add "page name" or "set heading" option or something
-# - [ ] If base-path is included, add a markdown link under the heading?
-#   - [ ] deprecate base-path arg, require url always and only download if file isn't supplied
+# - [ ] Fix anchor links
+# - [x] Fix heading issue
+# - [x] log and don't output for pages with empty CONTENT
+# - [x] change logging to not go to STDOUT. improve verbose logging
+# - [x] deprecate base-path arg, require url always and only download if file isn't supplied
 # - [x] Show help if no arguments are passed
 # - [x] Clean up code blocks
 # - [x] fix relative url paths
@@ -27,31 +30,48 @@ HELPMESSAGE=$(
   cat <<-END
   Converts web pages into (somewhat) nicely formatted markdown (WIP)
 Required:
-(Either url or file must be specified, not both)
 -u/--url - url to download and convert
--f/--file - file to convert
 
 Optional:
 -F/--format - is the pandoc format arg. Defaults to markdown_strict-raw_html+simple_tables
 -H/--no-heading - turns off adding the file name as a heading to the final markdown doc
--p/--pretty-heading - Formats filenames a bit prettier into the markdown headings i.e. foo_bar-baz.html -> # foo bar baz
 -v/--verbose - Show extra info when processing
---base-path <base-path> - Prepend non-absolute resource paths with a base path
-  i.e. with --base-path="https://google.com/foobar"
-  - src="images/foo.jpg" -> src="https://google.com/foobar/images/foo.jpg"
-  - src="/images/foo.jpg" -> src="https://google.com/images/foo.jpg"
+--error-on-empty - Exit with an error if there is no page content after being converted
+  Default behavior is to not output the page and log that it was empty, but exit successfully
 
 Dependencies:
 htmlq - preprocess html for better markdown conversion
 pandoc - Actual html to markdown conversion
-getopt (gnu version) - arg parsing
+getopt - arg parsing
 curl - Downloading page
+
+NOTE: This is not yet working cross platform. 
 END
 )
 
+# Initialize argument vars
+URL=""
+ADD_HEADING=true
+FORMAT="markdown_strict-raw_html+simple_tables"
+TEST_ARGS=false
+VERBOSE=false
+ERROR_ON_EMPTY=false
+
+log() {
+  # TODO: Add -s/--silent option
+  echo "main.sh: $@" >&2
+}
+
+verbose() {
+  # Output to STDERR if verbose flag is on
+  if [[ $VERBOSE == true ]]; then
+    log "main.sh: $@"
+  fi
+}
+
 fail() {
-  echo "$1"
-  echo "$0 terminating..."
+  log "$@"
+  log "main.sh terminating..."
   exit 1
 }
 
@@ -62,7 +82,7 @@ fi
 
 ## ARGUMENT HANDLING ##
 
-TEMP=$(getopt -o uphHvf:F: --longoptions url:,file:,format:,base-path:,heading,pretty-heading,test-args,verbose -n "$0" -- "$@")
+TEMP=$(getopt -o hHvF:u: --longoptions url:,format:,heading,test-args,verbose,error-on-empty -n "$0" -- "$@")
 
 # Exit if getopt has an error
 if [ $? != 0 ]; then
@@ -70,15 +90,6 @@ if [ $? != 0 ]; then
 fi
 
 eval set -- "$TEMP"
-
-FILE=""
-URL=""
-ADD_HEADING=true
-BASE_PATH=""
-FORMAT="markdown_strict-raw_html+simple_tables"
-PRETTY_HEADINGS=false
-TEST_ARGS=false
-VERBOSE=false
 
 # Ingest command line args
 while true; do
@@ -89,19 +100,22 @@ while true; do
     exit 0
     ;;
   # Required
-  -f | --file)
-    FILE=$2
-    shift 2 # Shift 2 to drop flag name & argument
-    ;;
   -u | --url)
     URL=$2
     shift 2
+
+    # FIX: I'm sure there are conditions where this won't work
+    # TODO: Add logging / validations
+    URL_DIR=$(echo "$URL" | sed -E 's|(.+)/[^/]*$|\1|' | sed 's|/$||')  # https://foobar.com/bar/bazfoo.html -> https://foobar.com/bar
+    URL_DOMAIN=$(echo "$URL" | sed -E -n 's|.*(https?://[^/]+).*|\1|p') # https://foo.bar/biz/buz.jpg -> https://foo.bar
+    # Used in heading and generating a file name with -o
+    URL_FILE=$(echo "$URL" | sed -E 's|.+/([^/]+)\..*$|\1|') # https://foo.bar/blog/2024/big-cool-article.html -> big-cool-article
     ;;
+  # Optional
   -F | --format)
     FORMAT=$2
     shift 2
     ;;
-  # Optional
   -v | --verbose)
     VERBOSE=true
     shift 1
@@ -110,17 +124,13 @@ while true; do
     ADD_HEADING=false
     shift 1 # Shift 1 only because it's just a on/off toggle with no value
     ;;
-  -p | --pretty-heading)
-    PRETTY_HEADINGS=true
+  --error-on-empty)
+    ERROR_ON_EMPTY=true
     shift 1
     ;;
   --test-args)
     TEST_ARGS=true
     shift 1
-    ;;
-  --base-path)
-    BASE_PATH=$2
-    shift 2
     ;;
   --)
     shift
@@ -135,93 +145,79 @@ done
 if [[ $TEST_ARGS == true ]]; then
   echo "Parsed argument string: $TEMP"
   echo "FORMAT: $FORMAT"
-  echo "FILE: $FILE"
+  echo "URL: $URL"
   echo "ADD_HEADING: $ADD_HEADING"
-  echo "PRETTY_HEADINGS: $PRETTY_HEADINGS"
+  echo "ERROR_ON_EMPTY: $ERROR_ON_EMPTY"
+  echo "VERBOSE: $VERBOSE"
   exit 0
 fi
-
-verbose() {
-  if [[ $VERBOSE == true ]]; then
-    echo "verbose message: $1"
-  fi
-}
 
 ## VALIDATIONS ##
 verbose "Starting argument validations"
 
-# Require either url or file, not both
-if [[ "$FILE" == "" && "$URL" == "" ]]; then
-  fail "A file (-f/--file) or URL (-u/--url) must be supplied. see --help"
-elif [[ "$FILE" != "" && "$URL" != "" ]]; then
-  fail "A file AND a url have been supplied. It can only be one or the other."
-fi
-
-# --base-path shouldn't be specified separately if --url is
-if [[ "$URL" != "" && $BASE_PATH != "" ]]; then
-  fail "both --url and --base-path have been specified, don't do that. The base url should be based off the full url of the page."
+# Require url parameter
+if [[ "$URL" == "" ]]; then
+  fail "A url have been supplied. It can only be one or the other."
 fi
 
 ## PROGRAM ##
 
 # Get html
-if [[ "$URL" != "" ]]; then
-  CONTENT=$(curl -L $URL)
+verbose "Grabbing html for $URL"
+HTML="$(curl -sSL $URL)"
+# verbose "$(echo $CONTENT | wc -l) lines of CONTENT"
 
-  if [ $? != 0 ]; then
-    fail "Failed to download page"
-  elif [[ "$CONTENT" == "" ]]; then
-    fail "No content to convert in the downloaded page."
+if [ $? != 0 ]; then
+  fail "Failed to download page"
+elif [[ "$HTML" == "" ]]; then
+  fail "No content to convert in the downloaded page."
+fi
+
+# Make a heading
+HEADING=""
+
+if [[ $ADD_HEADING == true ]]; then
+  PAGE_NAME="$(echo "$HTML" | htmlq 'div.page_title_bar' -r 'div.page_title_bar>*' | pandoc --from html --to $FORMAT --no-highlight)"
+
+  # Backup name for the page is a slightly cleaned up version of the url's slug
+  if [[ "$PAGE_NAME" == "" ]]; then
+    PAGE_NAME="$(echo $URL_FILE | sed 's/[-_]/ /g')"
   fi
 
-  BASE_PATH=$(echo "$URL" | sed -E 's|(.+)/[^/]*$|\1|')
-else
-  if [[ "$FILE" != "" && ! -f "$FILE" ]]; then
-    fail "File to convert not found: $FILE\nTerminating..."
-  fi
-
-  CONTENT=$(cat $FILE)
+  HEADING=$"# $PAGE_NAME [SOURCE]($URL)"$'\n'
 fi
 
 # Fix href/src references to relative urls
-if [[ $BASE_PATH != "" ]]; then
-  verbose "starting base path corrections"
+verbose "Correcting paths"
+HTML=$(
+  echo "$HTML" |
+    # TODO: Combine the href/src expressions
+    # TODO: Handle anchors i.e. src="#foo" -> src="$URL#foo" (with no slash between!)
+    # correct relative same site urls i.e. src="whatever.jpg"
+    sed -E "s|href=\"([^/][^:\"]+)\"|href=\"$URL_DIR/\1\"|g" |
+    sed -E "s|src=\"([^/][^:\"]+)\"|src=\"$URL_DIR/\1\"|g" |
+    # correct absolute same site urls i.e. src="/whatever.jpg"
+    sed -E "s|href=\"/([^\"]+)\"|href=\"$URL_DOMAIN/\1\"|g" |
+    sed -E "s|src=\"/([^\"]+)\"|src=\"$URL_DOMAIN/\1\"|g"
+)
 
-  verbose "getting base domain"
-  BASE_PATH=$(echo $BASE_PATH | sed 's/\/$//')                                # Ensure any trailing slashes are stripped
-  BASE_DOMAIN=$(echo $BASE_PATH | sed -E -n 's/.*(https?:\/\/[^\/]+).*/\1/p') # https://foo.bar/biz/buz.jpg -> https://foo.bar
+# Convert html content to markdown
+verbose "Converting page html to markdown"
+CONTENT="$(echo "$HTML" | htmlq 'div.page_content' -r '.rouge-gutter' | pandoc --from html --to "$FORMAT" --no-highlight)"
 
-  verbose "Correcting paths..."
-  CONTENT=$(
-    echo "$CONTENT" |
-      # TODO: Combine the href/src expressions
-      # correct relative same site urls i.e. src="whatever.jpg"
-      sed -E "s|href=\"([^/][^:\"]+)\"|href=\"$BASE_PATH/\1\"|g" |
-      sed -E "s|src=\"([^/][^:\"]+)\"|src=\"$BASE_PATH/\1\"|g" |
-      # correct absolute same site urls i.e. src="/whatever.jpg"
-      sed -E "s|href=\"/([^\"]+)\"|href=\"$BASE_DOMAIN/\1\"|g" |
-      sed -E "s|src=\"/([^\"]+)\"|src=\"$BASE_DOMAIN/\1\"|g"
-  )
-  verbose "Paths corrected"
-fi
-
-# Add heading
-# TODO: Fix heading arg flag logic
-if [[ $ADD_HEADING == true ]]; then
-  verbose "Adding heading"
-  if [[ $PRETTY_HEADINGS = true ]]; then
-    echo -e "# $(basename $FILE .html | sed "s/[-_]/ /g")\n"
+# Check for empty content
+if [[ "$(echo \"$CONTENT\" | sed '/^\s*$/d')" == "" ]]; then
+  if [[ "$ERROR_ON_EMPTY" == true ]]; then
+    fail "Converted content from page was empty (url: $URL)"
   else
-    if [[ "$URL" != "" ]]; then
-      # ... TODO: Add citation for source url
-    else
-      echo -e "# Converted HTML to $FORMAT from: $(basename $FILE)\n"
-    fi
+    log "Converted content from page was empty (url: $URL), omitting output"
+    exit 0
   fi
 fi
 
-# Convert html to markdown
-verbose "Converting page"
-echo "$CONTENT" |
-  htmlq 'div.page_content' -r '.rouge-gutter' |    # Extract and clean html
-  pandoc --from html --to "$FORMAT" --no-highlight # Convert to markdown
+# output converted content
+echo "$HEADING"
+echo "$CONTENT"
+
+# TODO: remove, this is a temporary fix for a specific use case
+echo # Add empty line at end
