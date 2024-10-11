20d00466fb1ec016ed40a12db3f8fff6be1e76e5
Cooper LeBrun <cooperlebrun@gmail.com>
2024-10-11 10:41:55 -0400

    Use stdin as html source if present

--- main.sh
+++ main.sh
@@ -9,7 +9,7 @@
 #     - [ ] 404
 #     - [ ] 403
 #     - [ ] 500
-# - [ ] Enable consuming html from stdin
+# - [x] Enable consuming html from stdin
 # - [x] Option for output to file with formatted name
 #   - [x] Add substitution for domain
 #   - [ ] Ask to make directory if -o specifies one.
@@ -37,7 +37,7 @@ Required:
 -u/--url - url to download and convert
 
 Optional:
--o/--output-file - Output to a file. Substitutes <domain> and <slug>
+-o/--output-file - Output to a file. Substitutes <domain> and <slug>. Only works if -u is supplied.
   - i.e. ./main.sh -u "https://google.com/foo/really_cool_page.html" -f "<domain>/<slug>.md" # -> google_com/really_cool_page.md
   - Doesn't make directories so this will fail if google_com doesn't already exist
 -F/--format - is the pandoc format arg. Defaults to markdown_strict-raw_html+simple_tables
@@ -57,20 +57,10 @@ curl - Downloading page
 END
 )
 
-# Exit if there are no arguments
-if [[ $@ == "" ]]; then
-  fail "$HELPMESSAGE"
-fi
+## LOGGING FUNCTIONS ##
 
-# Initialize argument vars
-URL=""
-ADD_HEADING=true
-FORMAT="markdown_strict-raw_html+simple_tables"
-TEST_ARGS=false
 VERBOSE=false
 SILENT=false
-ERROR_ON_EMPTY=false
-OUTPUT_FILE=""
 
 log() {
   # TODO: Add -s/--silent option
@@ -88,11 +78,31 @@ verbose() {
 
 fail() {
   log "$@"
-  log "main.sh terminating..."
+  log "terminating..."
   exit 1
 }
 
-## ARGUMENT HANDLING ##
+## INPUT HANDLING ##
+
+# Use STDIN if there is any
+STDIN=""
+if [[ ! -t 0 ]]; then # non blocking check for STDIN content
+  STDIN="$(cat -)"
+fi
+
+# Exit if there are no arguments
+if [[ "$@" == "" && "$STDIN" == "" ]]; then
+  echo "main.sh: $HELPMESSAGE"
+  exit 0
+fi
+
+# Initialize argument vars
+URL=""
+ADD_HEADING=true
+FORMAT="markdown_strict-raw_html+simple_tables"
+TEST_ARGS=false
+ERROR_ON_EMPTY=false
+OUTPUT_FILE=""
 
 TEMP=$(getopt -o shHvF:u:o: --longoptions url:,format:,output-file:,no-heading,test-args,verbose,error-on-empty,silent -n "$0" -- "$@")
 
@@ -175,16 +185,27 @@ fi
 
 verbose "Starting argument validations"
 
-# Require url parameter
-if [[ "$URL" == "" ]]; then
-  fail "A url have been supplied. It can only be one or the other."
+# Require a source of html
+if [[ "$URL" == "" && "$STDIN" == "" ]]; then
+  fail "No --url specified, and no html input through STDIN. Nothing to convert!"
+fi
+
+if [[ "$OUTPUT_FILE" != "" && "$URL" == "" ]]; then
+  if [[ "$(echo $OUTPUT_FILE | sed -E 's/(<slug>|<domain>)/\1/')" != "" ]]; then
+    fail "You specified an output file with tokens for <slug> or <domain>, but there's no url (-u) to get those from"
+  fi
 fi
 
 ## PROGRAM ##
 
 # Get html
-verbose "Grabbing html for $URL"
-HTML="$(curl -sSL $URL)"
+if [[ "$STDIN" == "" ]]; then
+  verbose "Fetching html from $URL"
+  HTML="$(curl -sSL $URL)"
+else
+  verbose "Reading html from STDIN"
+  HTML="$STDIN"
+fi
 
 if [ $? != 0 ]; then
   fail "Failed to download page"
@@ -199,34 +220,41 @@ if [[ $ADD_HEADING == true ]]; then
   PAGE_NAME="$(echo "$HTML" | htmlq 'div.page_title_bar' -r 'div.page_title_bar>*' | pandoc --from html --to $FORMAT --no-highlight)"
 
   # Backup name for the page is a slightly cleaned up version of the url's slug
-  if [[ "$PAGE_NAME" == "" ]]; then
+  if [[ "$PAGE_NAME" == "" && "$URL" != "" ]]; then
     PAGE_NAME="$(echo $URL_SLUG | sed 's/[-_]/ /g')"
   fi
 
-  HEADING=$"# $PAGE_NAME [SOURCE]($URL)"$'\n'
+  HEADING=$"# $PAGE_NAME"
+
+  if [[ "$URL" != "" ]]; then
+    HEADING+=" [SOURCE]($URL)"
+  fi
+
+  HEADING+=$'\n' # Add blank line under heading
 fi
 
-# Fix href/src references to relative urls
-verbose "Correcting paths"
-HTML=$(
-  echo "$HTML" |
-    # TODO: Combine the href/src expressions
-    # correct in-page anchor links
-    sed -E "s|href=\"(#[^\"]+)\"|href=\"$URL\1\"|g" |
-    # correct relative same site urls i.e. src="whatever.jpg"
-    sed -E "s|href=\"([^#/][^:\"]+)\"|href=\"$URL_DIR/\1\"|g" |
-    sed -E "s|src=\"([^#/][^:\"]+)\"|src=\"$URL_DIR/\1\"|g" |
-    # correct absolute same site urls i.e. src="/whatever.jpg"
-    sed -E "s|href=\"/([^\"#]+)\"|href=\"$URL_DOMAIN/\1\"|g" |
-    sed -E "s|src=\"/([^\"#]+)\"|src=\"$URL_DOMAIN/\1\"|g"
-)
+if [[ "$URL" != "" ]]; then
+  # Fix href/src references to relative urls
+  verbose "Correcting paths"
+  HTML=$(
+    echo "$HTML" |
+      # correct in-page anchor links
+      sed -E "s|href=\"(#[^\"]+)\"|href=\"$URL\1\"|g" |
+      # correct relative same site urls i.e. src="whatever.jpg"
+      sed -E "s|href=\"([^#/][^:\"]+)\"|href=\"$URL_DIR/\1\"|g" |
+      sed -E "s|src=\"([^#/][^:\"]+)\"|src=\"$URL_DIR/\1\"|g" |
+      # correct absolute same site urls i.e. src="/whatever.jpg"
+      sed -E "s|href=\"/([^\"#]+)\"|href=\"$URL_DOMAIN/\1\"|g" |
+      sed -E "s|src=\"/([^\"#]+)\"|src=\"$URL_DOMAIN/\1\"|g"
+  )
+fi
 
 # Convert html content to markdown
 verbose "Converting page html to markdown"
 CONTENT="$(echo "$HTML" | htmlq 'div.page_content' -r '.rouge-gutter' | pandoc --from html --to "$FORMAT" --no-highlight)"
 
 # Exit if content is empty
-if [[ "$(echo \"$CONTENT\" | sed '/^\s*$/d')" == "" ]]; then
+if [[ "$(echo $CONTENT | sed '/^\s*$/d')" == "" ]]; then
   if [[ "$ERROR_ON_EMPTY" == true ]]; then
     fail "Converted content from page was empty (url: $URL)"
   else
@@ -236,14 +264,16 @@ if [[ "$(echo \"$CONTENT\" | sed '/^\s*$/d')" == "" ]]; then
 fi
 
 if [[ "$OUTPUT_FILE" != "" ]]; then
-  echo "$URL_DOMAIN_ONLY"
-  # TODO: Check that required URL_* vars are not empty
-  # replace any substitution tokens
-  CLEAN_DOMAIN_NAME="$(echo $URL_DOMAIN_ONLY | sed 's/\./_/g')"
-  # TODO: ask to create folder if it's in a folder
-  OUTPUT_FILE="$(echo $OUTPUT_FILE |
-    sed $'s/<slug>/'$"$URL_SLUG"$'/g' |
-    sed $'s|<domain>|'$"$CLEAN_DOMAIN_NAME"$'|g')"
+  if [[ "$URL" != "" ]]; then
+    # TODO: Check that required URL_* vars are not empty
+    # replace any substitution tokens
+    CLEAN_DOMAIN_NAME="$(echo $URL_DOMAIN_ONLY | sed 's/\./_/g')"
+    # TODO: ask to create folder if it's in a folder
+    OUTPUT_FILE="$(echo $OUTPUT_FILE |
+      sed $'s/<slug>/'$"$URL_SLUG"$'/g' |
+      sed $'s|<domain>|'$"$CLEAN_DOMAIN_NAME"$'|g')"
+  fi
+
   echo "$HEADING" >>$OUTPUT_FILE
   echo "$CONTENT" >>$OUTPUT_FILE
 else # Otherwise echo to STDOUT
